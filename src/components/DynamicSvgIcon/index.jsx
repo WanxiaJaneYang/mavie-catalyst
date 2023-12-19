4212755e5ffac66facce8459c10fcf9a94b4da38
WanxiaJaneYang <1016887513@qq.com>
2023-12-19 12:55:58 +1030

    added fallback for failure url

--- src/components/DynamicSvgIcon/index.jsx
+++ src/components/DynamicSvgIcon/index.jsx
@@ -1,31 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import ReportProblemOutlinedIcon from '@mui/icons-material/ReportProblemOutlined';
 import { Box } from '@mui/material';
 
-function DynamicSvg({
-	svgData, ...props
-}) {
-	if (!svgData) {
-		return <ReportProblemOutlinedIcon {...props} />;
-	}
+function DynamicSvg({ svgData, ...props }) {
+	const [hasError, setHasError] = useState(false);
 
 	const imgStyle = {
 		width: '100%',
 		height: '100%',
-		objectFit: 'contain', // This makes sure the image keeps its aspect ratio
+		objectFit: 'contain',
 	};
+
+	const handleOnError = () => {
+		setHasError(true);
+	};
+
+	if (!svgData || hasError) {
+		return <ReportProblemOutlinedIcon {...props} />;
+	}
 
 	return (
 		<Box {...props}>
-			<img src={svgData} alt="Dynamic SVG" style={imgStyle} />
+			<img
+				src={svgData}
+				alt="Dynamic SVG"
+				onError={handleOnError}
+				style={imgStyle}
+			/>
 		</Box>
 	);
 }
 
 DynamicSvg.propTypes = {
 	svgData: PropTypes.string.isRequired,
-
 };
 
 export default DynamicSvg;
