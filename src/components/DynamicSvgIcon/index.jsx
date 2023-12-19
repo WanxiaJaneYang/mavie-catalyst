17b2a5695b845c4a91e2a1f823a3411d99e5080a
WanxiaJaneYang <1016887513@qq.com>
2023-12-19 11:41:25 +1030

    uploaded the change from svg node to url

--- src/components/DynamicSvgIcon/index.jsx
+++ src/components/DynamicSvgIcon/index.jsx
@@ -3,35 +3,29 @@ import PropTypes from 'prop-types';
 import ReportProblemOutlinedIcon from '@mui/icons-material/ReportProblemOutlined';
 import { Box } from '@mui/material';
 
-const isValidSvg = (data) => {
-	const parser = new DOMParser();
-	const doc = parser.parseFromString(data, 'image/svg+xml');
-	return doc.documentElement.nodeName.toLowerCase() === 'svg';
-};
-
-// This component directly renders the SVG data
-
-function DynamicSvg({ svgData, ...props }) {
-	// Check if the SVG data is valid
-	if (!isValidSvg(svgData)) {
-		// Fallback to a default icon if the SVG data is invalid
+function DynamicSvg({
+	svgData, ...props
+}) {
+	if (!svgData) {
 		return <ReportProblemOutlinedIcon {...props} />;
 	}
-	// Directly set the SVG XML as the inner HTML of this component
+
+	const imgStyle = {
+		width: '100%',
+		height: '100%',
+		objectFit: 'contain', // This makes sure the image keeps its aspect ratio
+	};
 
 	return (
 		<Box {...props}>
-			<div
-				className="svg-container"
-				dangerouslySetInnerHTML={{ __html: svgData }}
-			/>
-			{/* {' '} */}
+			<img src={svgData} alt="Dynamic SVG" style={imgStyle} />
 		</Box>
 	);
 }
 
 DynamicSvg.propTypes = {
 	svgData: PropTypes.string.isRequired,
+
 };
 
 export default DynamicSvg;
